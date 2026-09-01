81d3947a117c6c8c489e8d08ebe0f0b9023ee882
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-01 14:48:56 +0200

    Add project files.

---
+++ C# Basics/Assignment.1_C.sharp02/Assignment.1_C.sharp02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using System.Reflection;
+using System.Timers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+class Book
+{
+    public string Title;
+    public int Pages;
+}
+class program
+{
+    static void Main()
+    {
+      
+    }
+}
+
